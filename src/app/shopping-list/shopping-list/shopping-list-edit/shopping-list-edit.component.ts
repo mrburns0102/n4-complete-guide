6a4dace6570c6f2bca8648c15fa495ca1000d54e
Dragan Dosen <dragan.dosen@synechron.com>
2021-05-28 15:45:56 +0200

    Recipies adventures

--- src/app/shopping-list/shopping-list/shopping-list-edit/shopping-list-edit.component.ts
+++ src/app/shopping-list/shopping-list/shopping-list-edit/shopping-list-edit.component.ts
@@ -1,9 +1,20 @@
-import { Component } from "@angular/core";
+import { Component, ElementRef, EventEmitter, ViewChild } from "@angular/core";
+import { Ingredient } from "src/app/shared/ingredient.model";
 
 @Component({
   selector: 'app-shopping-list-edit',
   templateUrl: "./shopping-list-edit.component.html"
 })
 export class ShoppingListEditComponent {
+  @ViewChild('nameInput') nameInputRef: ElementRef;
+  @ViewChild('amountInput') amountInputRef: ElementRef;
+  ingredientAdded = new EventEmitter<Ingredient>()
+  
+  constructor() {};
 
+  ngOnInit() {}
+
+  onAddItem() {
+
+  }
 }
